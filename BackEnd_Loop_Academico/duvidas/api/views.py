3123be40c97f61d623ab5f1b5b3d23c51bc0a611
gersonbrenof <gersonfagundes2016@gmail.com>
2024-08-22 21:23:25 -0300

    ataçizaçao em duvida

--- BackEnd_Loop_Academico/duvidas/api/views.py
+++ BackEnd_Loop_Academico/duvidas/api/views.py
@@ -10,13 +10,8 @@ class DuvidasViewSet(viewsets.ModelViewSet):
     queryset = Duvidas.objects.all()
     serializer_class = DuvidasSerializer
     permission_classes = [IsAuthenticated]
-    http_method_names = ['get', 'post']
+    http_method_names = ['post']
 
     def perform_create(self, serializer):
         anonimo = serializer.validated_data.get('anonimo', True)
         serializer.save(usuario=self.request.user.aluno, anonimo=anonimo)
-
-    def get_queryset(self):
-        if self.request.user.is_staff:
-            return Duvidas.objects.all()
-        return Duvidas.objects.filter(aluno=self.request.user.aluno)
