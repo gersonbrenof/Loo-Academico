c4049f5eb324e143cd78155c3326d3de7a90338a
gersonbrenof <gersonfagundese2016@gmail.com>
2025-07-17 16:45:47 -0300

    adcionado permissao nos cors

--- BackEnd_Loop_Academico/BackEnd_Loop_Academico/settings.py
+++ BackEnd_Loop_Academico/BackEnd_Loop_Academico/settings.py
@@ -94,6 +94,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     'http://localhost:3000',
     "http://127.0.0.1:5500",
+    'https://loop-academic-front.onrender.com',
 
 ]
 CORS_ALLOW_CREDENTIALS = True
