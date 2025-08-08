3b46e623c377e636d2367523ecdf9362f8ce75ff
gersonbrenof <gersonfagundese2016@gmail.com>
2025-08-08 11:17:57 -0300

    adcionando permissao para amply

--- BackEnd_Loop_Academico/BackEnd_Loop_Academico/settings.py
+++ BackEnd_Loop_Academico/BackEnd_Loop_Academico/settings.py
@@ -95,6 +95,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     "http://127.0.0.1:5500",
     'https://loop-academic-front.onrender.com',
+    'https://main.d2qzjqtihdf05j.amplifyapp.com',
 
 ]
 CORS_ALLOW_CREDENTIALS = True
